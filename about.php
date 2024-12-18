10dd8f5bc35da458338f003d560063fdcb4dfd7d
hzimhzwnn <hzimhzwnn@gmail.com>
2024-12-18 10:12:03 +0800

    Update about.php

--- about.php
+++ about.php
@@ -35,7 +35,6 @@
                     <h2>INFO</h2>
                     <p>CURRENT PATIENTS: 33 PERSONS</p>
                     <p>VOLUNTEERS: 7 PERSONS</p>
-                    <p>aa</p>
                 </div>
             </div>
         </div>
